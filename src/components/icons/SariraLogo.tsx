2bf98c4d9991295073ad7562de4455ba9cf7968d
RAJESHWARAN cs040 <rajeshwarancs40@gmail.com>
2025-08-04 05:04:02 +0000

    replace this image

--- src/components/icons/SariraLogo.tsx
+++ src/components/icons/SariraLogo.tsx
@@ -7,7 +7,7 @@ interface SariraLogoProps extends React.HTMLAttributes<HTMLDivElement> {}
 export const SariraLogo = React.forwardRef<HTMLDivElement, SariraLogoProps>(({ className, ...props }, ref) => (
     <div ref={ref} className={cn("relative", className)} {...props}>
         <Image
-            src="https://storage.googleapis.com/stedi-dev-images/sarira-logo-updated.png"
+            src="https://storage.googleapis.com/stedi-dev-images/sarira_logo_black.png"
             alt="SARIRA Logo"
             width={120}
             height={45}
