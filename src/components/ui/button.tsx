86295d749c6e9aedb550fc8704ddce4eff527a84
RAJESHWARAN cs040 <rajeshwarancs40@gmail.com>
2025-08-19 11:27:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
+import { Slot, Slottable } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
@@ -47,7 +47,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        <Slottable>{props.children}</Slottable>
+      </Comp>
     )
   }
 )
