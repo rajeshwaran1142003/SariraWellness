5d3ab773a16e1ea4be9b197df29f8c183dcd0e7f
RAJESHWARAN cs040 <rajeshwarancs40@gmail.com>
2025-08-20 09:51:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons/benefits/ImmunityBoosterIcon.tsx
+++ src/components/icons/benefits/ImmunityBoosterIcon.tsx
@@ -11,5 +11,5 @@ export const ImmunityBoosterIcon = (props: SVGProps<SVGSVGElement>) => (
   >
     <path d="M51.5,3.5H12.5c-2.8,0-5,2.2-5,5v50c0,2.8,2.2,5,5,5h39c2.8,0,5-2.2,5-5V8.5C56.5,5.7,54.3,3.5,51.5,3.5z M45.8,32.3 L35,43.1c-0.6,0.6-1.5,0.6-2.1,0L22.2,32.3c-0.6-0.6-0.6-1.5,0-2.1l2.1-2.1c0.6-0.6,1.5-0.6,2.1,0l4,4l4-4c0.6-0.6,1.5-0.6,2.1,0 l2.1,2.1C46.4,30.8,46.4,31.7,45.8,32.3z" />
   </svg>
-));
+);
 ImmunityBoosterIcon.displayName = "ImmunityBoosterIcon";
